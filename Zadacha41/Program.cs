4fa3696a7faad0d15e67fe1451dba63f6d1906f2
Igor Popkov <i.popkov@CPSURAL.LOCAL>
2022-12-06 10:19:32 +0500

    задача 41

--- Zadacha41/Program.cs
+++ Zadacha41/Program.cs
@@ -3,16 +3,51 @@
 1, -7, 567, 89, 223-> 3 */
 
 
-Console.Write("Введите колличество чисел: ");
-int n = int.Parse(Console.ReadLine());
+Console.Write("Введите числа через запятую: ");
+string? input = Console.ReadLine();
+char splitSymbol = ',';
 int count = 0;
-for (int i = 0; i < n; i++)
-{
-    Console.Write($"Введите число {i + 1}: ");
-    int x = int.Parse(Console.ReadLine());
-    if (x > 0)
-        count++;
-}
-Console.WriteLine($"Положительных чисел: {count}");
 
+WriteArray(ParseArray(input, splitSymbol));
+
+int[] ParseArray(string inputNumbers, char split)
+{
+    int numbersCount = 1;
+    for(int i = 0; i < inputNumbers.Length; i++)
+{
+    if(inputNumbers[i] == split)
+    numbersCount++;
+}
+int[] numbers = new int[numbersCount];
+int numberIndex = 0;
+string subString = "";
+
+for(int i = 0; i < inputNumbers.Length; i++)
+{
+    if(inputNumbers[i] == split)
+{
+    numbers[numberIndex++] = Convert.ToInt32(subString);
+    subString = "";
+}
+else
+{
+    subString += inputNumbers[i];
+}
+}
+    numbers[numberIndex] = Convert.ToInt32(subString);
+    return numbers;
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " " );
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(" =>" + count);
+}
 
